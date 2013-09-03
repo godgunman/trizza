7ef838f4435d9ccbc6aaea7e9ed8f6bc5e14006a
godgunman <godgunman@gmail.com>
2013-09-04 03:47:40 +0800

    update deploy.sh, adding force push command

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 git remote add heroku git@heroku.com:trizza.git
 git subtree push --prefix web heroku master
+
+# http://stackoverflow.com/questions/12644855/how-do-i-reset-a-heroku-git-repository-to-its-initial-state
+# git push heroku `git subtree split --prefix web master`:master --force
+
