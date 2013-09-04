9a617122780812ba63a8ee28c552374b3bd35bad
telgniw <b97902044@ntu.edu.tw>
2013-09-04 12:02:51 +0800

    Update donate theme and add subscribe button.

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,8 +3,9 @@
  */
 
 exports.index = function(req, res){
+    var coinbase_id = 'f37bd28ae4360948711344c4118db83b';
     res.render('index', {
         title: 'Trizza',
-        subtitle: 'Crowdsourced testing for your app.',
+        coinbase_id: coinbase_id,
     });
 };
